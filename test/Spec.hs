386bbdca83651d8d633bdd8aaee4ab5eabb90f83
Gustavo Bicalho <gusbicalho@gmail.com>
2019-07-18 23:25:25 -0300

    remove comments

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,1 @@
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
--- {-# LANGUAGE
---     OverloadedStrings
---   #-}
--- {- HLINT ignore "Redundant do" -}
--- module Main (main) where
-
--- import Test.Hspec
-
--- import qualified ServantTest.HttpApi.UserSpec
--- import qualified VersionSpec
-
--- main :: IO ()
--- main = hspec $ do
---   VersionSpec.spec
---   ServantTest.HttpApi.UserSpec.spec
