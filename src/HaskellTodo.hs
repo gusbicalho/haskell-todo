8799c81f3f4ff17eeeda9973f7a551ea96925a90
Gustavo Bicalho <gusbicalho@gmail.com>
2019-08-09 16:20:24 -0300

    extract HaskellTodo.WarpServer and run server in parallel

--- src/HaskellTodo.hs
+++ src/HaskellTodo.hs
@@ -1,38 +1,15 @@
-module HaskellTodo
-    ( startApp
-    ) where
+module HaskellTodo (
+  startApp
+  ) where
 
-import Network.Wai.Handler.Warp
-import Network.Wai.Logger
-import Control.Monad.Reader
-import Servant
-
-import qualified HaskellTodo.Config as Config
-import qualified HaskellTodo.Env as Env
-import qualified HaskellTodo.HttpApi as HttpApi
+import Control.Concurrent.Async ( withAsync, wait )
+import HaskellTodo.Config ( loadConfig )
+import HaskellTodo.Env ( buildEnv )
+import qualified HaskellTodo.WarpServer as WarpServer
 
 startApp :: IO ()
 startApp = do
-  config <- Config.loadConfig
-  env <- Env.buildEnv config
-  startHttpApi env
-
-type ReadEnvT m = ReaderT Env.Env m
-
-type AppM = ReadEnvT Handler
-
-withSettingsFromEnv :: Env.Env -> (Settings -> IO a) -> IO a
-withSettingsFromEnv env actionFn = withStdoutLogger $ \aplogger ->
-    actionFn $ setPort (Config.port $ #config env)
-             . setLogger aplogger
-             $ defaultSettings
-
-startHttpApi :: Env.Env -> IO ()
-startHttpApi env = do
-    let port = #port . #config $ env
-    putStrLn $ "Server running at port " ++ show port
-    withSettingsFromEnv env $ \settings ->
-      runSettings settings (HttpApi.app env provideDependencies)
-  where
-    provideDependencies :: AppM x -> Handler x
-    provideDependencies m = runReaderT m env
+  config <- loadConfig
+  env <- buildEnv config
+  withAsync (WarpServer.run env) $ \warp ->
+    wait warp
---
+++ src/HaskellTodo/WarpServer.hs
@@ -0,0 +1,30 @@
+module HaskellTodo.WarpServer (
+    run
+  ) where
+
+import Network.Wai.Handler.Warp hiding ( run )
+import Network.Wai.Logger
+import Control.Monad.Reader
+import Servant
+
+import qualified HaskellTodo.Config as Config
+import qualified HaskellTodo.Env as Env
+import qualified HaskellTodo.HttpApi as HttpApi
+
+type WarpAppM = ReaderT Env.Env Handler
+
+withSettingsFromEnv :: Env.Env -> (Settings -> IO a) -> IO a
+withSettingsFromEnv env actionFn = withStdoutLogger $ \aplogger ->
+    actionFn $ setPort (Config.port $ #config env)
+             . setLogger aplogger
+             $ defaultSettings
+
+run :: Env.Env -> IO ()
+run env = do
+    let port = #port . #config $ env
+    putStrLn $ "Server running at port " ++ show port
+    withSettingsFromEnv env $ \settings ->
+      runSettings settings (HttpApi.app env provideDependencies)
+  where
+    provideDependencies :: WarpAppM x -> Handler x
+    provideDependencies m = runReaderT m env
